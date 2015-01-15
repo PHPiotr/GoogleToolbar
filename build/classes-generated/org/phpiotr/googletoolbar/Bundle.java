b56eb2e1f05bc2266add0d6a2ad5cf32c745c4f9
Piotr Kowalski <pkowalski@localhost.localdomain>
2015-01-15 22:11:25 +0100

    Create simple module for Netbeans IDE - Google Search Toolbar

---
+++ build/classes-generated/org/phpiotr/googletoolbar/Bundle.java
@@ -0,0 +1,13 @@
+package org.phpiotr.googletoolbar;
+/** Localizable strings for {@link org.phpiotr.googletoolbar}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Google</i>
+     * @see GoogleActionListener
+     */
+    static String CTL_GoogleActionListener() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_GoogleActionListener");
+    }
+    private void Bundle() {}
+}
